dc256fc78735ad5c0bc8ba3d989fa3e9e8eb9547
Gabriel Tiossi <gabrieltiossi@gmail.com>
2020-05-08 04:55:09 -0300

    chore: black formatting

--- botaclan/constants.py
+++ botaclan/constants.py
@@ -1,8 +1,10 @@
 from vyper import v
 
-v.set_env_prefix('botaclan')
-v.set_env_key_replacer('.', '_')
+v.set_env_prefix("botaclan")
+v.set_env_key_replacer(".", "_")
 
-v.bind_env('discord.token')
+v.bind_env("discord.guild")
+v.bind_env("discord.token")
 
-DISCORD_TOKEN = v.get('discord.token')
+DISCORD_GUILD = v.get("discord.guild")
+DISCORD_TOKEN = v.get("discord.token")
