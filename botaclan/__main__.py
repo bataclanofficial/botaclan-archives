d4032a4833017dab1bc92b6263de91db501b524d
Gabriel Tiossi <gabrieltiossi@gmail.com>
2020-05-08 06:40:57 -0300

    feat: move from client to bot
    Change the library usage from client to bot to make it more extendable

--- botaclan/__main__.py
+++ botaclan/__main__.py
@@ -1,18 +1,19 @@
+from discord.ext import commands
 import botaclan.constants
 import discord
 import logging
 
-client = discord.Client()
+bot = commands.Bot(command_prefix=botaclan.constants.COMMAND_PREFIX)
 
 
-@client.event
+@bot.event
 async def on_ready():
     guild = discord.utils.find(
-        lambda g: g.id == botaclan.constants.DISCORD_GUILD_ID, client.guilds
+        lambda g: g.id == botaclan.constants.DISCORD_GUILD_ID, bot.guilds
     )
     logging.info(
-        f"{client.user} connected to the following guild {guild.name}({guild.id}):"
+        f"{bot.user} connected to the following guild {guild.name}({guild.id}):"
     )
 
 
-client.run(botaclan.constants.DISCORD_TOKEN)
+bot.run(botaclan.constants.DISCORD_TOKEN)
--- botaclan/constants.py
+++ botaclan/constants.py
@@ -4,12 +4,15 @@ import logging
 v.set_env_prefix("botaclan")
 v.set_env_key_replacer(".", "_")
 
+v.bind_env("command.prefix")
 v.bind_env("discord.guild.id")
 v.bind_env("discord.token")
 v.bind_env("log.level")
 
+v.set_default("command.prefix", "~b")
 v.set_default("log.level", logging.INFO)
 
+COMMAND_PREFIX = v.get_string("command.prefix")
 DISCORD_GUILD_ID = v.get_int("discord.guild.id")
 DISCORD_TOKEN = v.get_string("discord.token")
 LOG_LEVEL = v.get_int("log.level")
