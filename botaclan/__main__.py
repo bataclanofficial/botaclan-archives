b56d48bd464fba25aaea61e5e647637c64f66fba
Gabriel Tiossi <gabrieltiossi@gmail.com>
2020-05-16 16:20:35 -0300

    refactor: remove any logging.level() references

--- botaclan/__main__.py
+++ botaclan/__main__.py
@@ -3,6 +3,8 @@ import botaclan.constants
 import discord
 import logging
 
+log = logging.getLogger(__name__)
+
 bot = commands.Bot(
     command_prefix=botaclan.constants.COMMAND_PREFIX, case_insensitive=True
 )
@@ -18,9 +20,7 @@ async def on_ready():
     guild = discord.utils.find(
         lambda g: g.id == botaclan.constants.DISCORD_GUILD_ID, bot.guilds
     )
-    logging.info(
-        f"{bot.user} connected to the following guild {guild.name}({guild.id}):"
-    )
+    log.info(f"{bot.user} connected to the following guild {guild.name}({guild.id}):")
 
 
 bot.run(botaclan.constants.DISCORD_TOKEN)
