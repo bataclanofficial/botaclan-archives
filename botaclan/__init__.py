832d114a442a2c7be49f7cacfd9ce272c73257f5
Gabriel Tiossi <gabrieltiossi@gmail.com>
2020-05-16 16:02:46 -0300

    feat: sentry inital setup
    Rename log_constants function and sample sentry-sdk

--- botaclan/__init__.py
+++ botaclan/__init__.py
@@ -1,19 +1,24 @@
-import coloredlogs
+from importlib_metadata import version
 import botaclan.constants
+import coloredlogs
 import logging
+import sentry_sdk
 import sys
 
-log_format = logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(message)s")
+__version__ = version(__package__)
 
 root_log = logging.getLogger()
 root_log.setLevel(botaclan.constants.LOG_LEVEL)
 coloredlogs.install(
     logger=root_log, stream=sys.stdout, level=botaclan.constants.LOG_LEVEL
 )
-
+logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(message)s")
 logging.getLogger("asyncio").setLevel(botaclan.constants.LOG_ASYNCIO_LEVEL)
 logging.getLogger("discord").setLevel(botaclan.constants.LOG_DISCORD_LEVEL)
 logging.getLogger("websockets").setLevel(botaclan.constants.LOG_WEBSOCKETS_LEVEL)
 logging.getLogger(__name__)
 
-botaclan.constants.log_configuration()
+if botaclan.constants.SENTRY_ENABLED:
+    sentry_sdk.init(dsn=botaclan.constants.SENTRY_DSN)
+
+botaclan.constants.log_constants()
--- botaclan/constants.py
+++ botaclan/constants.py
@@ -23,10 +23,11 @@ def set_env_defaults():
     v.set_default("log.discord.level", "warning")
     v.set_default("log.level", "info")
     v.set_default("log.websockets.level", "warning")
+    v.set_default("sentry.enabled", False)
     v.set_default("timezone", "America/Sao_Paulo")
 
 
-def log_configuration():
+def log_constants():
     log.debug(f"command.prefix={COMMAND_PREFIX}")
     log.debug(f"discord.guild.id={DISCORD_GUILD_ID}")
     log.debug(f"discord.token={DISCORD_TOKEN}")
@@ -38,6 +39,8 @@ def log_configuration():
     log.debug(f"log.discord.level={LOG_DISCORD_LEVEL}")
     log.debug(f"log.level={LOG_LEVEL}")
     log.debug(f"log.websockets.level={LOG_WEBSOCKETS_LEVEL}")
+    log.debug(f"sentry.enabled={SENTRY_ENABLED}")
+    log.debug(f"sentry.dsn={SENTRY_DSN}")
     log.debug(f"timezone={TIMEZONE}")
 
 
@@ -55,4 +58,6 @@ LOG_ASYNCIO_LEVEL = v.get_string("log.asyncio.level").upper()
 LOG_DISCORD_LEVEL = v.get_string("log.discord.level").upper()
 LOG_LEVEL = v.get_string("log.level").upper()
 LOG_WEBSOCKETS_LEVEL = v.get_string("log.websockets.level").upper()
+SENTRY_ENABLED = v.get_string("sentry.enabled")
+SENTRY_DSN = v.get_string("sentry.dsn")
 TIMEZONE = v.get_string("timezone")
