bb0a06a02c72af896f35733c0212529b4209b4e2
Gabriel Tiossi <gabrieltiossi@gmail.com>
2020-05-27 20:27:44 -0300

    refactor: move on_ready to Botaclan class
    Move on_ready from an event annotation in __main__.py to Botaclan class
    in bot.py

--- botaclan/__main__.py
+++ botaclan/__main__.py
@@ -1,14 +1,12 @@
 from botaclan.bot import Botaclan
 from botaclan.constants import (
     COMMAND_PREFIX,
-    DISCORD_GUILD_ID,
     DISCORD_TOKEN,
     SENTRY_DSN,
     SENTRY_ENABLED,
 )
 import botaclan.constants
 import botaclan.healthcheck.server
-import discord
 import logging
 import sentry_sdk
 
@@ -22,13 +20,6 @@ if SENTRY_ENABLED:
 
 bot = Botaclan(command_prefix=COMMAND_PREFIX, case_insensitive=True)
 server = botaclan.healthcheck.server.create_server()
-
-
-@bot.event
-async def on_ready():
-    guild = discord.utils.find(lambda g: g.id == DISCORD_GUILD_ID, bot.guilds)
-    log.info(f"{bot.user} connected to the following guild {guild.name}({guild.id}):")
-
 
 botaclan.bot.running_bot = bot
 bot.loop.create_task(server.serve())
--- botaclan/bot.py
+++ botaclan/bot.py
@@ -1,11 +1,14 @@
 from botaclan.constants import (
-    FEATURE_ROULETTE,
+    DISCORD_GUILD_ID,
     FEATURE_CALENDAR,
+    FEATURE_ROULETTE,
 )
 from discord.ext.commands import Bot
+import discord.utils
 import asyncio
 import logging
 import signal
+
 
 log = logging.getLogger(__name__)
 
@@ -21,6 +24,12 @@ class Botaclan(Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.load_cogs()
+
+    async def on_ready(self):
+        guild = discord.utils.find(lambda g: g.id == DISCORD_GUILD_ID, self.guilds)
+        log.info(
+            f"{self.user} connected to the following guild {guild.name}({guild.id}):"
+        )
 
     def load_cogs(self):
         for cog, status in COGS_AND_FEATURE_STATUS.items():
