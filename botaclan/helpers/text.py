6e844b3e76c84e86ebdf150a70ca991e8198f9a1
Gabriel Tiossi <gabrieltiossi@gmail.com>
2020-05-27 22:07:10 -0300

    refactor(helpers): use validators from django.core
    Migrate validate_email_address from regex usage to django, while also
    creating the validate_url function from the same lib

--- botaclan/helpers/text.py
+++ botaclan/helpers/text.py
@@ -1,11 +1,24 @@
+from django.core.validators import URLValidator, ValidationError, EmailValidator
 from typing import List
-import re
 
 
 def parse_comma_list_message(message: str) -> List[str]:
     return [item.strip() for item in message.split(",")]
 
 
+def validate_url(url: str) -> bool:
+    validate = URLValidator()
+    try:
+        validate(url)
+        return True
+    except ValidationError:
+        return False
+
+
 def validate_email_address(email: str) -> bool:
-    email_regex = r"^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$"
-    return bool(re.search(email_regex, email))
+    validate = EmailValidator()
+    try:
+        validate(email)
+        return True
+    except ValidationError:
+        return False
--- tests/helpers/test_text.py
+++ tests/helpers/test_text.py
@@ -18,3 +18,15 @@ def test_validate_email_address_non_existing_mail():
     sent = "not.working.hello.com"
     expected = False
     assert botaclan.helpers.text.validate_email_address(sent) == expected
+
+
+def test_validate_url_existent():
+    sent = "https://youtu.be/myrandomidhere"
+    expected = True
+    assert botaclan.helpers.text.validate_url(sent) == expected
+
+
+def test_validate_url_inexistent():
+    sent = "this should not https://work.here"
+    expected = False
+    assert botaclan.helpers.text.validate_url(sent) == expected
